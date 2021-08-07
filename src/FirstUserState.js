51179be2f972af69a5b30e3a385a61052fa8d8bf
Senkottuvelan <34165508+Senkottuvelan@users.noreply.github.com>
2021-08-07 11:16:46 +0530

    Some Minor corrections

--- src/FirstUserState.js
+++ src/FirstUserState.js
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function FirstUserState() {
     const supabaseUrl = 'https://vdwqdynfeifvlczhjfqz.supabase.co'
-    const supabaseKey = process.env.SUPABASE_KEY
+    const supabaseKey = process.env.REACT_APP_SUPABASE_KEY
     const supabase = createClient(supabaseUrl, supabaseKey)
 
 
